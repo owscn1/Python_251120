e8a3d5e5e8dc01da32988c96590ef2d3dd5e787f
younayeon <owscn1@gmail.com>
2025-11-20 22:32:53 +0900

    week 12

---
+++ w12.py
@@ -0,0 +1,49 @@
+# 32
+a = {1, 2, 3, 4, 5}
+b = {6, 7, 8, 9, 10}
+x = a | b
+y = a & b
+print(x, y)
+
+# 33
+a = {1, 2, 3, 4, 5}
+b = {6, 7, 8, 9, 10}
+x = a - b
+y = a ^ b
+print(x, y)
+
+# 34
+a = {1, 2, 3, 4, 5}
+a |= {100}
+print(a)
+
+# 35
+a = {100, 200, 300, 400, 500}
+b = {400, 500, 600, 700, 800}
+x = a.copy()
+x &= b
+y = a.copy()
+y -= b
+z = a.copy()
+z ^= b
+print(x, y, z)
+
+# 36
+a = {100, 200, 300, 400, 500}
+b = {100, 200, 300, 400, 500}
+if  a >= b and a <= b:
+    print("동시")
+elif a >= b:
+    print("상위")
+elif a <= b:
+    print("부분")
+
+# 37
+a = {1, 2 ,3, 4, 5}
+a.add(1000)
+a.remove(1000)
+print(a)
+
+# 38
+multiples = { x for x in range(1, 101) if x % 3 == 0 and x % 5 == 0}
+print(multiples)
